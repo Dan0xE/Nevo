8a3b560d8887cbb631b879169ac21cbf2181c6cc
Dan0xE <danopperm@gmail.com>
2023-03-09 03:58:12 +0100

    overwrite existing files by default

--- src-tauri/src/utils/copy_snapshot.rs
+++ src-tauri/src/utils/copy_snapshot.rs
@@ -22,13 +22,19 @@ fn copy_files(source: &str, destination: &str) -> io::Result<()> {
         if entry.file_type()?.is_file() {
             let destination_file = destination_path.join(entry.file_name());
 
-            if (destination_file).exists() && entry.file_name() == "glfw.dll" {
-                println!("Seems like a snapshot already exist, exiting loop");
-                break;
-            }
-
             // Copy the file to destination
-            fs::copy(entry_path, destination_file)?;
+            fs::copy(&entry_path, &destination_file)
+                .map_err(|e| {
+                    if e.kind() == io::ErrorKind::AlreadyExists {
+                        println!("File already exists, overwriting...");
+                        fs::remove_file(&destination_file)?;
+                        fs::copy(entry_path, destination_file)?;
+                        Ok(())
+                    } else {
+                        Err(e)
+                    }
+                })
+                .unwrap();
         }
     }
 
--- src-tauri/src/utils/generate_args.rs
+++ src-tauri/src/utils/generate_args.rs
@@ -5,6 +5,17 @@ use super::is_minecraft_running::is_minecraft_running;
 
 pub(crate) fn generate_args() -> bool {
     let current_dir = current_dir().unwrap_or_default();
+
+    let args_txt_path = current_dir.join("args.txt");
+    let args_bat_path = current_dir.join("args.bat");
+
+    if args_txt_path.exists() {
+        std::fs::remove_file(args_txt_path).unwrap();
+    }
+    if args_bat_path.exists() {
+        std::fs::remove_file(args_bat_path).unwrap();
+    }
+
     let file_path = current_dir.join("p.ps1");
     if is_minecraft_running() {
         match run(file_path.into_os_string().into_string().unwrap().as_str()) {
