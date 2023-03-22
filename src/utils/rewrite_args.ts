936c95d64856e2111f252ff76fcbbbe72721ddca
Dan0xE <danopperm@gmail.com>
2023-03-22 02:53:30 +0100

    removed external powershell script and inlined it

--- src/utils/rewrite_args.ts
+++ src/utils/rewrite_args.ts
@@ -27,57 +27,55 @@ function copy_files(old_path: string, new_path: string) {
 export default function rewrite_args(username: string, path: string) {
   invoke("generate_args_command").then((res) => {
     if (res) {
-      setTimeout(() => {
-        invoke("read_args_command").then((res) => {
-          let args = shiftArrary(res as string[], 2);
+      invoke("read_args_command").then((res) => {
+        let args = shiftArrary(res as string[], 2);
 
-          if (args.length < 3) {
-            alert("Failed to generate args");
-            return;
+        if (args.length < 3) {
+          alert("Failed to generate args");
+          return;
+        }
+
+        for (let i = 0; i < args.length; i++) {
+          if (args[i].includes("-Djava.library.path")) {
+            let old = args[i];
+            args[i] = "-Djava.library.path=" + path;
+            let new_path_log = args[i];
+            console.log(args[i]);
+            console.log(`Original Path before modification is: ${old}`);
+            console.log(`New Path is ${new_path_log}`);
+
+            old = old.substring(20);
+
+            copy_files(old, path);
           }
+        }
 
-          for (let i = 0; i < args.length; i++) {
-            if (args[i].includes("-Djava.library.path")) {
-              let old = args[i];
-              args[i] = "-Djava.library.path=" + path;
-              let new_path_log = args[i];
-              console.log(args[i]);
-              console.log(`Original Path before modification is: ${old}`);
-              console.log(`New Path is ${new_path_log}`);
-
-              old = old.substring(20);
-
-              copy_files(old, path);
+        for (let i = 0; i < args.length; i++) {
+          if (args[i].includes("--username")) {
+            if (username) {
+              args[i + 1] = username;
+              console.log(args[i + 1]);
+            } else {
+              alert("No username specified");
             }
           }
+        }
 
-          for (let i = 0; i < args.length; i++) {
-            if (args[i].includes("--username")) {
-              if (username) {
-                args[i + 1] = username;
-                console.log(args[i + 1]);
-              } else {
-                alert("No username specified");
-              }
-            }
-          }
+        let store = args.join(" ");
 
-          let store = args.join(" ");
-
-          console.log(store);
-          if (path) {
-            console.log(path);
-            invoke("write_args_command", {
-              args: store,
-              path: path,
-            }).then(() => {
-              invoke("launch_game_command");
-            });
-          } else {
-            alert("No path specified");
-          }
-        });
-      }, 15000);
+        console.log(store);
+        if (path) {
+          console.log(path);
+          invoke("write_args_command", {
+            args: store,
+            path: path,
+          }).then(() => {
+            invoke("launch_game_command");
+          });
+        } else {
+          alert("No path specified");
+        }
+      });
     } else {
       console.error("Failed to generate args");
       alert("failed to generate args, maybe you forgot to run minecraft?");
