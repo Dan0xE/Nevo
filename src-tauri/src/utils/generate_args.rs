67ae2e80f03dd50c3d05e47e2f0661801efe1324
Dan0xE <danopperm@gmail.com>
2023-03-22 02:44:44 +0100

    fixed bug that would prevent namechanges | introduced shortcuts

--- src-tauri/src/utils/generate_args.rs
+++ src-tauri/src/utils/generate_args.rs
@@ -1,5 +1,7 @@
 use std::env::current_dir;
 
+use crate::utils::create_shortcut::create_symlimk;
+
 use super::{argument_wrapper::argument_wrapper, is_minecraft_running::is_minecraft_running};
 
 pub(crate) fn generate_args() -> bool {
