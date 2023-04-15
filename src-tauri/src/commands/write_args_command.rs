3f3a8b40cb8ab1e5425065d056439249961a2f98
Dan0xE <danopperm@gmail.com>
2023-04-15 22:20:33 +0200

    cleanup

--- src-tauri/src/commands/write_args_command.rs
+++ src-tauri/src/commands/write_args_command.rs
@@ -5,14 +5,11 @@ pub(crate) fn write_args_command(args: String) -> Result<String, String> {
     match write_args(args) {
         Ok(_) => {
             println!("Wrote Arguments");
-            //no idea why we have to use unwrap here
-            Ok::<String, String>("sucess".to_string()).unwrap();
+            return Ok::<String, String>("sucess".to_string());
         }
         Err(e) => {
             println!("Failed to write: {:?}", e);
-            //no idea why we have to use unwrap here
-            Err::<String, String>(e.to_string()).unwrap();
+            return Err::<String, String>(e.to_string());
         }
     };
-    Ok("sucess".to_string())
 }
